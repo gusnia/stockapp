b89d47edf56cd490df2d7d9e365c11bee617fc1e
Gusnia <rahmagusnia7@gmail.com>
2026-05-06 14:35:01 +0700

    Add mock data for sharia stocks endpoint

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,18 @@ Route::get('/debug-db', function () {
 
 
 Route::get('/sharia-stocks', function () {
-    $companies = \App\Models\Company::passesShariaScreening()->get();
+    $mockData = [
+        'BBCA' => ['price' => 9450, 'change' => 2.34, 'volume' => '128.4M', 'market_cap' => '1.17T', 'sector' => 'Keuangan'],
+        'TLKM' => ['price' => 3820, 'change' => -1.03, 'volume' => '89.2M', 'market_cap' => '376.8B', 'sector' => 'Teknologi'],
+        'UNVR' => ['price' => 20000, 'change' => 0.85, 'volume' => '62.7M', 'market_cap' => '213.5B', 'sector' => 'Konsumer'],
+        'ASII' => ['price' => 5275, 'change' => 1.46, 'volume' => '45.1M', 'market_cap' => '121.3B', 'sector' => 'Otomotif'],
+        'ICBP' => ['price' => 10450, 'change' => -2.18, 'volume' => '31.6M', 'market_cap' => '98.7B', 'sector' => 'Konsumer'],
+    ];
+
+    $companies = \App\Models\Company::passesShariaScreening()->get()->map(function ($company) use ($mockData) {
+        $mock = $mockData[$company->ticker] ?? ['price' => 0, 'change' => 0, 'volume' => '0', 'market_cap' => '0', 'sector' => '-'];
+        return array_merge($company->toArray(), $mock);
+    });
+
     return response()->json(['data' => $companies]);
 });
