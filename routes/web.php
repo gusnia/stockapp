3ea09933a3cbf988b68c936d989036f409bac602
gusnia <rahmagusnia7@gmail.com>
2026-05-06 12:15:57 +0700

    fix: update env configuration

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 
 Route::get('/', [MarketController::class, 'watch'])->name('market.watch');
 Route::get('/market', [MarketController::class, 'watch'])->name('market.watch');
+
+Route::get('/debug-env', function () {
+    return [
+        'db_host' => env('DB_HOST'),
+        'db_name' => env('DB_DATABASE'),
+        'db_user' => env('DB_USERNAME'),
+    ];
+});
